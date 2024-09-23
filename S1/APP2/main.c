e228c370c3d8e8feff056e23deac6f14c50e7a5d
ZwoukLeBleu <88751650+ZwoukLeBleu@users.noreply.github.com>
2024-09-23 16:21:09 -0400

    1

--- S1/APP2/main.c
+++ S1/APP2/main.c
@@ -1,11 +1,11 @@
 //ceci est un test mega genial
-struct test {
-    int a;
-    int b;
-    matrix[a][b];
-}
 
 int main(){
 
+    struct test {
+        int a;
+        int b;
+        matrix[a][b];
+    }
     return 0;
 }
