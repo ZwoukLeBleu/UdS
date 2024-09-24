c4c5b031fa25e70cf3e94b2773500cafc4101768
ZwoukLeBleu <zakaryromdhane@gmail.com>
2024-09-23 23:42:38 -0400

    1

--- S1/APP2/main.c
+++ S1/APP2/main.c
@@ -10,9 +10,18 @@
     return 0;
 }*/
 #include <stdio.h>
+
+struct test {
+        int a;
+        int b;
+        char c;
+};
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+    
+   struct test t1 = {1,2,'g'};
+   putchar(t1.c);
+   printf("\n");
    return 0;
 }
 
