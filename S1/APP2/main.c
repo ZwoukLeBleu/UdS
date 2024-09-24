c0d64b6d8626ffdffe075bebc1f582ec6f6cd41b
ZwoukLeBleu <zakaryromdhane@gmail.com>
2024-09-24 16:09:35 -0400

    sin and cos functional

--- S1/APP2/main.c
+++ S1/APP2/main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include "main.h"
 
-#define MAX_SIN_N 10
+#define MAX_TAYLOR_I 10
 
 
 struct matrixS {
@@ -19,7 +19,7 @@ struct matrixS {
 //KINDA DONE
 int charFind(char a, char str[]){
     int i;
-    for (i = 0; i < sizeof(str); i++) {
+    for (i = 0; i < sizeof(str); i++) { //TODO: fix gcc qui chialle sur sizeof(char[])
         if (str[i] == a){
             return i;
         }
@@ -30,13 +30,11 @@ int charFind(char a, char str[]){
 
 
 //detection de palindrome
-//WIP
-int palindromeCheck(char str[], int p){
-    
-    if (str[p-1] == str[sizeof(str)-p]){
-        return 1;   
-    }
-    else return 0;
+//DONE
+int palindromeRecur(char string[], int i, int j){
+    if (i >= j) { return 0; } //return TRUE si les 2 'indexes' se croisent
+    if (string[i]!=string[j]) { return 1; }
+    return palindromeRecur(string, i+1, j-1);
     
 }
 
@@ -51,8 +49,8 @@ int factorial(int x){
     else { return -1; }
 }
 
-int power(int x, int p){
-    int t = 1;
+float power(float x, int p){
+    float t = 1;
     for (p; p > 0; p--){
         t *= x;
     }
@@ -61,16 +59,30 @@ int power(int x, int p){
 
 
 //sin ~= x - x3/3! + x5/5!
-//WIP
-float sinNear(int x){
+//DONE, NOT OPTIMISED
+float sinNear(float x){
     float t = 0;
-    for (int i = 1; i < 20; i+=2){
+    int v=1;
+    for (int i = 1; i < MAX_TAYLOR_I; i+=2){
         //printf(" %d",i);
-        t += power(-1,i)*(power(x,i)/factorial(i));
-        printf(" %d",t);
+        t += power(-1,v)*(power(x, i)/factorial(i));
+        v++;
     }
     return t;
-    
+}
+
+
+//sin ~= 1 - x2/2! + x4/5!
+//DONE, NOT OPTIMISED
+float cosNear(float x){
+    float t = 0;
+    int v=1;
+    for (int i = 2; i < MAX_TAYLOR_I; i+=2){
+        //printf(" %d",i);
+        t += power(-1,v)*(power(x, i)/factorial(i));
+        v++;
+    }
+    return t+1;
 }
 
 /*void matrixAdd(struct matrixS m1, struct matrixS m2){
@@ -87,15 +99,13 @@ float sinNear(int x){
 
 int main() {
 
-   //struct test t1 = {1,2,'g'};
-   //putchar(t1.c);
-   //putchar(charFinder('E', greets));
-    //char greets[] = "TENET";
-    //int wt = palindromeCheck(greets, sizeof(greets));
-    //printf("%d",wt);
+    char gaming[] = "RACECAR";
+    int wtf = palindromeRecur(gaming, 0, 7-1); 
+    //printf("%d",wtf);
 
     float test = sinNear(3);
     printf("%.6f", test);
+    //printf("%d", power(-1,3));
     //int g = power(5,4);
     //printf("%d", g);
 
--- S1/APP2/main.h
+++ S1/APP2/main.h
@@ -4,9 +4,9 @@
 
 
 int charFind(char a, char str[]);
-int palindromeCheck(char str[], int p);
+int palindromeRecur(char string[], int i, int j);
 int factorial(int x);
-int power(int x, int p);
-float sinNear(int x);
+float power(float x, int p);
+float sinNear(float x);
 
 #endif
