a9e23f046127728ef03249d0d2d2289fc141073e
ZwoukLeBleu <zakaryromdhane@gmail.com>
2024-09-30 18:18:39 -0400

    matrixadd no struct

--- S1/APP2/Code/additionMatrices.c
+++ S1/APP2/Code/additionMatrices.c
@@ -9,6 +9,11 @@ Description: Fonctions qui font de l'aritmetique & des operations sur le matrice
 #include <stdio.h>
 
 #define MAX_MATRIX_SIZE 99
+#define MATRIX1_M 2
+#define MATRIX1_N 3
+#define MATRIX2_M 2
+#define MATRIX2_N 3
+int mR[MATRIX1_M][MATRIX1_N] = {0};
 
 /*typedef struct Matrix {
         int m;
@@ -19,10 +24,10 @@ Description: Fonctions qui font de l'aritmetique & des operations sur le matrice
 // Description  : affiche une matrice à l'écran
 // Précondition : matrice non-nulle
 // Postcondition: N/A
-void printMatrix(Matrix m0){
-    for (int i = 0; i < m0.m; i++){
-        for (int j = 0; j < m0.n; j++){
-            int x = m0.table[i][j];
+void printMatrix(int m0[MATRIX1_M][MATRIX1_N]){
+    for (int i = 0; i < MATRIX1_M; i++){
+        for (int j = 0; j < MATRIX1_N; j++){
+            int x = m0[i][j];
             printf("%d ", x);
         }
     printf("\n");
@@ -33,51 +38,46 @@ void printMatrix(Matrix m0){
 // Précondition : pour recevoir un resultat, les matrices doivent etre de meme taille
 //                Les matrices doivent etre plus petites que MAX_MATRIX_SIZE 
 // Postcondition: N/A
-Matrix matrixAdd(Matrix m1, Matrix m2){
-    if (m1.m == m2.m && m1.n == m2.n){
-        Matrix mR = {m1.m, m1.n};
-        for (int i = 0; i <= m1.m; i++){
-            for (int j = 0; j <= m1.n; j++){
-                mR.table[i][j] = m1.table[i][j] + m2.table[i][j];
+void matrixAdd(int m1[MATRIX1_M][MATRIX1_N], int m2[MATRIX2_M][MATRIX2_N]){
+    if (MATRIX1_M == MATRIX2_M && MATRIX1_N ==  MATRIX2_N){
+        
+        for (int i = 0; i <= MATRIX1_M; i++){
+            for (int j = 0; j <= MATRIX1_N; j++){
+                mR[i][j] = m1[i][j] + m2[i][j];
             }
         }
-        return mR;
+        //return mR;
     //printMatrix(mR);
     }
     else{
-        return m1;
+        //return m1;
         //printf("Matrice(s) de mauvaise(s) taille(s)!");
     }
 }
-int matrixEquality(Matrix m1, Matrix m2){
-    if (m1.m == m2.m && m1.n == m2.n){
-        for (int i = 0; i <= m1.m; i++){
-            for (int j = 0; j <= m1.n; j++){
-                if (m1.table[i][j] != m2.table[i][j]){
-                    return 0;
-                }
+int matrixEquality(int m1[MATRIX1_M][MATRIX1_N], int m2[MATRIX1_M][MATRIX1_N]) {
+    for (int i = 0; i < MATRIX1_M; i++) {
+        for (int j = 0; j < MATRIX1_N; j++) {
+            if (m1[i][j] != m2[i][j]) {
+                return 0;
             }
         }
-        return 1;
     }
-    else{
-        return 0;
-    }
+    return 1;
 }
 
-void matrixAddTest(Matrix m1, Matrix m2, Matrix mR){
-    if (matrixEquality(matrixAdd(m1, m2), mR) == 1){
+void matrixAddTest(int mR[MATRIX1_M][MATRIX2_N], int mT[MATRIX1_M][MATRIX2_N]) {
+    if (matrixEquality(mR, mT) == 1) {
         printf("Test passed\n");
     } else {
         printf("Test failed\n");
     }
-
 }
 
 int main(){
-    Matrix m1 = {3, 2, {{1, 2}, {3, 4}, {5, 6}}};
-    Matrix m2 = {3, 2, {{6,5}, {4,3}, {2,1}}};
-    Matrix mR = {3, 2, {{7, 7}, {7, 7}, {7, 7}}};
-
-    matrixAddTest(m1, m2, mR);
+    int m1[MATRIX1_M][MATRIX1_N] = {{1,2,3}, {4,5,6}};
+    int m2[MATRIX2_M][MATRIX2_N] = {{6,5,4}, {3,2,1}};
+    int mT[MATRIX1_M][MATRIX2_N] = {{7,7,7}, {7,7,7}};
+    
+    matrixAdd(m1, m2);
+    matrixAddTest(mT, mR);
 }
