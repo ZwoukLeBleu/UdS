9f273152b89ac95963ff4188d50d1af5c4b00832
Mai181 <maina.clermont@gmail.com>
2024-09-30 21:19:23 -0400

    m

--- S1/APP2/Code/rechercheCharatere.c
+++ S1/APP2/Code/rechercheCharatere.c
@@ -3,7 +3,7 @@ Fichier: rechercheCaractere
 Auteurs: Zakary Romdhane romz6050
          Maïna Clermont clem5406
 Date: 24/09/2024
-Description: Fonctions qui font de l'aritmetique & des operations sur le matrices de base. Ne necessite pas d'input par l'utilisateur.
+Description: Fonctions pour la recherche d'un caractère dans une chaîne de caractère
 ********/
 
 #include <stdio.h>
@@ -32,17 +32,22 @@ int charFind(char a, char str[]){
     return -1;
 }
 
-charFindTest(char a, char str[], int val){
+void charFindTest(char a, char str[], int val){
     if (charFind(a, str) == val){
-        printf("%c in %s at i=%d : Test passed\n", a, str, val);
+		printf("'%c' est recherche dans %s. Le resultat attendu est %d.\n", a, str, val);
+        printf("Resultat obtenu : %d -> Test passed !\n", charFind(a, str));
     } else {
-        printf("%c in %s at i= %d : Test failed\n", a, str, val);
+		printf("'%c' est recherche dans %s. Le resultat attendu est %d.\n", a, str, val);
+        printf("Resultat obtenu : %d -> Test failed !\n", charFind(a, str));
     }
 }
 
 int main(){
-    charFindTest('a', "racecar", 1);
-    charFindTest('g', "gaming", 0);
-    charFindTest('w', "ALLO", -1);
-    charFindTest('Y', "salutAvecUnY", 11);
+	char str1[] = "anticonstitutionnellement\0";
+	char str2[] = "bonjour\0";
+	char str3[] = "allocommentcava\0";
+    charFindTest('n', str1, 1);
+    charFindTest('e', str2, -1);
+    charFindTest('r', str2, 6);
+    charFindTest('a', str3, 0);
 }
